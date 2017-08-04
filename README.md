39198a57ad0288eb25ddca62ee2242ccc0ef45b5
Fabricio C Zuardi <fabricio@fabricio.org>
2017-08-04 09:08:17 -0300

    more build system boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # videochat-client
-Embeddeable web app for video chat helpdesk. To be used with the videochat-api server.
+Embeddeable web app for video chat helpdesk. To be used with the [videochat-api][api] server.
+
+## Install
+
+```
+npm install
+```
+
+## Build bundle and launch local webserver (TBD)
+```
+npm start
+```
+
+## Contributing
+
+### Launch dev local webserver (TBD)
+```
+npm start dev
+```
+
+[api]: https://github.com/fczuardi/videochat-api
--- package-scripts.js
+++ package-scripts.js
@@ -1,5 +1,30 @@
+const { concurrent } = require("nps-utils");
+
+const transforms = ["unflowify", "tomlify", "es2040"].map(
+    t => "transform " + t
+);
+
+const appArgs = [
+    "entry src/app.js",
+    "outfile docs/app.js",
+    "no-bundle-external",
+    ...transforms
+];
+
 module.exports = {
   scripts: {
+    default: 'nps bundle && http-server docs',
+    dev: {
+        app: "budo --dir src src/app.js"
+    },
+    bundle: {
+        default: concurrent.nps(
+            "bundle.app",
+            "bundle.vendors"
+        ),
+        app: "browserify " + ["", ...appArgs].join(" --"),
+        vendors: ''
+    },
     test: 'flow',
     fmt: 'prettier --write --tab-width 4'
   }
