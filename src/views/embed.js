c04a8564af8972c41a58b1c43d60261b40c490bb
Frederico Marques <frederico.marques.22@gmail.com>
2017-08-05 20:11:44 -0300

    change embed view

--- src/views/embed.js
+++ src/views/embed.js
@@ -1,14 +1,19 @@
 // @flow
 import type { ChooView } from "../app";
 const html = require("choo/html");
+const messages = require("../messages");
 
-const homeView: ChooView = () => html`
-<div>
-    <iframe
-        src="https://tokbox.com/embed/embed/ot-embed.js?embedId=6e6ac6ff-c40a-433c-8c86-0198cc7fee5c&room=DEFAULT_ROOM&iframe=true"
-        width=800
-        height=640>
-    </iframe>
+const homeView: ChooView = (state, emit) => {
+    const requestRoom = (event) => {
+        emit("api:room")
+    }
+    console.log({state})
+    return html`
+<div id="videos">
+    <div id="publisher"></div>
+    <div id="subscriber"></div>
+    <button onclick=${requestRoom}>${messages.embed.call}</button>
 </div>`;
+}
 
 module.exports = homeView;
