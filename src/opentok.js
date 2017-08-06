0a95dd58708d629a7a1be3a4182ce44c98341037
Frederico Marques <frederico.marques.22@gmail.com>
2017-08-05 22:08:40 -0300

    add opentok

---
+++ src/opentok.js
@@ -0,0 +1,39 @@
+const OT = require('@opentok/client');
+// Handling all of our errors here by alerting them
+function handleError(error) {
+  if (error) {
+    alert(error.message);
+  }
+}
+
+function initializeSession({apiKey, sessionId, token}) {
+  if(!apiKey||!sessionId||!token) {
+    return 
+  }
+  var session = OT.initSession(apiKey, sessionId);
+  // Connect to the session
+  session.connect(token, function(error) {
+    // If the connection is successful, publish to the session
+    if (error) {
+      handleError(error);
+    } else {
+      // Subscribe to a newly created stream
+      session.on('streamCreated', function (event) {
+        // Create a publisher
+        var publisher = OT.initPublisher('publisher', {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%'
+        }, handleError);
+        session.publish(publisher, handleError);
+        session.subscribe(event.stream, 'subscriber', {
+          insertMode: 'append',
+          width: '100%',
+          height: '100%'
+        }, handleError);
+      });
+    }
+  });
+}
+
+module.exports = initializeSession
