ea05cff13a0285bbefdcc31a862040eb018074a6
Frederico Marques <frederico.marques.22@gmail.com>
2017-08-06 12:00:06 -0300

    add ui reducers for embed

---
+++ src/embedui.js
@@ -0,0 +1,19 @@
+// @flow
+import type { ChooMiddleware } from "./app";
+
+// click(call) -> requesting
+// requesting -> waiting (esperando o outro peer)
+// requesting -> connected (outro peer conectado)
+
+type RoomStatus = "disconnected" | "requesting" | "waiting" | "connected"
+const uiReducer: ChooMiddleware = (state, emitter) => {
+    state.roomStatus = "disconnected";
+    emitter.on("room:update", newStatus => {
+        state.roomStatus = newStatus
+        if(newStatus !== "connected") {
+            emitter.emit(state.events.RENDER)
+        }
+    })
+}
+
+module.exports = uiReducer;
