b2f08e54d65d052287503b25920095e16c4f09e6
Fabricio C Zuardi <fabricio@fabricio.org>
2017-08-09 16:34:29 -0300

    routes for ghpages

--- src/embed.js
+++ src/embed.js
@@ -17,6 +17,7 @@ app.use(errorReducer);
 app.use(chatReducer);
 app.route("*", defaultView);
 app.route("#group/:groupId", embedView);
+app.route("/videochat-client/embed.html#group/:groupId", embedView);
 
 if (typeof document === "undefined" || !document.body) {
     throw new Error("document.body is not here");
